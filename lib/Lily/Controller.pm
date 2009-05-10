eef527949e840add946dfcca4e1309cde9331514
Kan Fushihara <kan@kan-loox.(none)>
2009-05-10 23:01:24 +0900

    T::MTを使ったテンプレート機構をとりあえず用意。 最終的にはLily::Templateとして分離して切り替え可能にする。

--- lib/Lily/Controller.pm
+++ lib/Lily/Controller.pm
@@ -3,6 +3,11 @@ use strict;
 use warnings;
 use utf8;
 
+use String::CamelCase ();
+use Text::MicroTemplate::File;
+use HTTP::Engine::Response;
+use FindBin;
+
 sub import {
     my $pkg = caller(0);
     strict->import;
@@ -20,7 +25,26 @@ sub import {
     *{"$pkg\::run"} = sub {
         my ($self, $action, $request) = @_;
 
-        *{"$pkg\::on_$action"}->($self, $request);
+        my $res = *{"$pkg\::on_$action"}->($self, $request);
+        if ( !$res or ref($res) ne 'HTTP::Engine::Response' ) {
+            my $response = HTTP::Engine::Response->new;
+            $response->status(200);
+
+            my $tmpl_path = $pkg;
+            $tmpl_path =~ s!Lily::Controller::(Root)?!!g;
+            $tmpl_path = join('/', map { String::CamelCase::decamelize($_) } split /::/, $tmpl_path);
+
+            my $mtf= Text::MicroTemplate::File->new(
+                include_path => [ "$FindBin::Bin/assets/tmpl" ],
+                use_cache    => 1,
+            );
+
+            $response->body($mtf->render_file("$tmpl_path/$action.html", $request, $res)->as_string);
+            return $response;
+        }
+        else {
+            return $res;
+        }
     }
 
 }
--- lib/Lily/Controller/Root.pm
+++ lib/Lily/Controller/Root.pm
@@ -4,11 +4,7 @@ use Lily::Controller;
 on 'index' => sub {
     my ($self, $request) = @_;
 
-    my $res = HTTP::Engine::Response->new;
-    $res->status(200);
-    $res->body("run index");
-
-    $res;
+    { hello => 'world' };
 };
 
 1;
